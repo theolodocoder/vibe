5054d47c7147f995263114fff50824e38ef4f9f2
theolodocoder <theolodocoder@gmail.com>
2024-02-18 00:46:44 +0100

    feat:clerk auth setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-   <p className="text-red-500">Hello Vibe
-   </p>
-   <Button> Click</Button>
+      <p className="text-red-500">
+        Hello Vibe on authenticated user can see this :)
+        <UserButton afterSignOutUrl="/" />
+      </p>
     </>
   );
 }
